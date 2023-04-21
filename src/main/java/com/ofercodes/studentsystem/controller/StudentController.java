35b5a17a0a6f80466b89d315e5e90d5f6ba0d617
ofer <ofergavri@gmail.com>
2023-04-21 20:41:49 +0300

    implemented post and get requests

--- src/main/java/com/ofercodes/studentsystem/controller/StudentController.java
+++ src/main/java/com/ofercodes/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.ofercodes.studentsystem.controller;
 import com.ofercodes.studentsystem.model.Student;
 import com.ofercodes.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,10 @@ public class StudentController {
     public String addS(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "new student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/ofercodes/studentsystem/service/StudentService.java
+++ src/main/java/com/ofercodes/studentsystem/service/StudentService.java
@@ -2,6 +2,9 @@ package com.ofercodes.studentsystem.service;
 
 import com.ofercodes.studentsystem.model.Student;
 
+import java.util.List;
+
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
